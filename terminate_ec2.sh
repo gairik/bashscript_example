37a7499f308a4579983298cb3064235d3ac7089f
Gairik Aluni <aluni@campus.tu-berlin.de>
2020-08-23 16:22:05 +0200

    Add terminate ec2 instances

---
+++ terminate_ec2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+AWS_DEFAULT_OUTPUT=json
+readarray FILES <<< "$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --output text)"
+
+for inst in ${FILES[@]} 
+do
+    echo $inst
+    aws ec2 terminate-instances --instance-ids $inst
+done
